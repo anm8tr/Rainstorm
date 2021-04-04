19cd1cb7b4aad37c4b96dfb57c137503070e2bdc
Richard Garrison <threeduser@hotmail.com>
2021-04-03 22:33:47 -0700

    Project Setup

---
+++ Rainstorm/View Controllers/Root ViewController/RootViewController.swift
@@ -0,0 +1,21 @@
+//
+//  RootViewController.swift
+//  Rainstorm
+//
+//  Created by Richard Garrison on 4/3/21.
+//
+
+import UIKit
+
+final class RootViewController: UIViewController {
+    
+     // Mark: - View Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+
+}
+
